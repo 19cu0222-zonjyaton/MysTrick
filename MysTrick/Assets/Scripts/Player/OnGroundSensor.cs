bcf9bcbc85c4878286ce974141b9183f95b83761
ZonJyaton <19cu0222@jec.ac.jp>
2021-04-23 10:45:27 +0900

    ファイルの追加

---
+++ MysTrick/Assets/Scripts/Player/OnGroundSensor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnGroundSensor : MonoBehaviour
+{
+    public CapsuleCollider capcol;
+    public PlayerInput pi;
+    public GoalController goal;
+    public CameraController ca;
+
+    private Vector3 point1;
+    private Vector3 point2;
+    private float radius;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        radius = capcol.radius;
+
+        pi = gameObject.GetComponentInParent<PlayerInput>();    //  親のComponentを獲得する
+
+        goal = GameObject.Find("Goal").GetComponent<GoalController>();
+
+        ca = GameObject.Find("Main Camera").GetComponent<CameraController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        point1 = transform.position + transform.up * radius;                                            //  最高点
+        point2 = transform.position + transform.up * capcol.height - transform.up * radius * 3.5f;      //  最低点
+
+        Collider[] outputCols = Physics.OverlapCapsule(point1, point2 ,radius, LayerMask.GetMask("Ground"));        //  Groundという名前の階層と当てますか
+
+        if (outputCols.Length == 0)
+        {
+            pi.inputEnabled = false;
+            //foreach (var col in outputCols)
+            //{
+            //    print("collision: " + col.name);
+            //}
+        }
+        else if (outputCols.Length != 0 && !pi.lockJumpStatus && !goal.gameClear && ca.cameraStatic == "Idle")
+        {
+            pi.inputEnabled = true;
+        }
+    }
+}
