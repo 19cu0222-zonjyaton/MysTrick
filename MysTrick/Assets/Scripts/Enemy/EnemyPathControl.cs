a9f1d4fd7d40470540f2f8dc0ee20cf053ecc5a5
Jo Kan <qzzua925822122@gmail.com>
2021-05-23 21:57:37 +0900

    Enemy 素材更新 + 索敌视野更新

--- MysTrick/Assets/Scripts/Enemy/EnemyPathControl.cs
+++ MysTrick/Assets/Scripts/Enemy/EnemyPathControl.cs
@@ -19,17 +19,41 @@ public class EnemyPathControl : MonoBehaviour
 
     bool LockOn()
     {
-        if (System.Math.Abs(GameObject.Find("PlayerHandle").GetComponent<Transform>().position.y - transform.position.y) >= 1.0f)
+        if (System.Math.Abs(GameObject.Find("PlayerHandle").GetComponent<Transform>().position.y - transform.position.y) >= 2.0f)
             islock = false;
         else
         {
-            if (Vector3.Distance(GameObject.Find("PlayerHandle").GetComponent<Transform>().position , transform.position) >= field)
+            Vector3 targetPosition = GameObject.Find("PlayerHandle").GetComponent<Transform>().position;
+            if (Vector3.Distance(targetPosition, transform.position) >= field)
                 islock = false;
             else
-                islock = true;
+            {
+                float angle = 0.0f;
+                float headAngle;
+                float oppositeAngle = 0.0f;
+                headAngle = head.transform.localEulerAngles.y;
+                if (targetPosition.z < transform.position.z)
+                {
+                    oppositeAngle = Mathf.Acos((targetPosition.x - transform.position.x) / Vector3.Distance(targetPosition, transform.position)) * Mathf.Rad2Deg;
+                }
+                else if (targetPosition.z > transform.position.z)
+                {
+                    oppositeAngle = -Mathf.Acos((targetPosition.x - transform.position.x) / Vector3.Distance(targetPosition, transform.position)) * Mathf.Rad2Deg;
+                }
+                if (oppositeAngle <= 0 && oppositeAngle >= -180.0f)
+                {
+                    oppositeAngle = 360 + oppositeAngle;
+                }
+                angle = oppositeAngle - headAngle + 90.0f;
+                if (angle <= 0.0f && angle >= -60.0f || angle <= 60.0f && angle > 0.0f || angle <= 360.0f && angle >= 300.0f)
+                    islock = true;
+                else
+                    islock = false;
+            }
+
         }
         return islock;
-	}
+    }
     void Patrol()
     {
         transform.Translate((pathPositions[index].position - transform.position).normalized * Time.deltaTime * speed );
