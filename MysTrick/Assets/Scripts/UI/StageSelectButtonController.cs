5e56a1367b817529ac70f843cd15c1761a401376
Sou Hi <19cu0220@jec.ac.jp>
2021-06-14 22:42:38 +0900

    关卡选择

---
+++ MysTrick/Assets/Scripts/UI/StageSelectButtonController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class StageSelectButtonController : MonoBehaviour
+{
+    public bool canSelected;
+    public Button btn;
+    public Image img;
+    private Vector3 startPos;
+
+    void Awake()
+    {
+        img = gameObject.GetComponent<Image>();
+
+        btn = gameObject.GetComponent<Button>();
+
+        btn.onClick.AddListener(StageSelect);
+
+        startPos = transform.parent.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!canSelected)
+        {
+            img.color = Color.grey;
+        }
+        else
+        {
+            img.color = Color.white;
+        }
+
+        if (EventSystem.current.currentSelectedGameObject.name == "Stage04")
+        {
+            gameObject.transform.parent.position = Vector3.MoveTowards(transform.parent.position, startPos - new Vector3(300.0f, 0.0f, 0.0f), 500.0f * Time.deltaTime);
+        }
+        else if (EventSystem.current.currentSelectedGameObject.name == "Stage03")
+        {
+            gameObject.transform.parent.position = Vector3.MoveTowards(transform.parent.position, startPos, 500.0f * Time.deltaTime);
+        }
+    }
+
+    public void StageSelect()
+    {
+        if (canSelected)
+        {
+            if (gameObject.name == "Stage01")
+            {
+                SceneManager.LoadScene("Stage01");
+            }
+            else if (gameObject.name == "Stage02")
+            {
+                SceneManager.LoadScene("Stage02");
+            }
+            else if (gameObject.name == "Stage03")
+            {
+                SceneManager.LoadScene("Stage03");
+            }
+            else if (gameObject.name == "Stage04")
+            {
+                SceneManager.LoadScene("Stage04");
+            }
+        }
+    }
+}
