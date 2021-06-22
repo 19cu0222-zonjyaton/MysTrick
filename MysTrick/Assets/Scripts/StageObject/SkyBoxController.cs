17d85918f1bf4de8afc9e4bba420e39f436714c5
Sou Hi <19cu0220@jec.ac.jp>
2021-06-23 01:18:50 +0900

    选关界面天空盒

---
+++ MysTrick/Assets/Scripts/StageObject/SkyBoxController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBoxController : MonoBehaviour
+{
+    public Material[] boxs;
+    public ActorInStageSelect aiss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < boxs.Length; i++)
+        {
+            if (aiss.skyboxIndex == i)
+            {
+                RenderSettings.skybox = boxs[i];
+            }
+        }
+    }
+}
