f885593beae7e22998f3f7a79eb51f5ae08c2764
Sou Hi <19cu0220@jec.ac.jp>
2021-07-08 20:27:19 +0900

    金币百分比

---
+++ MysTrick/Assets/Scripts/UI/StageSelect/CoinCountController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class CoinCountController : MonoBehaviour
+{
+    public int[] nowCount;
+    public int[] highCount;
+    public float sumCount;
+    public int stageNum;
+    private Text textFrame;
+
+    void Awake()
+    {
+        textFrame = gameObject.GetComponent<Text>();
+    }
+
+    void Start()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (stageNum == i)
+            {
+                textFrame.text = (Math.Round((GoalController.getCount[stageNum] / sumCount) * 100.0f, 0)).ToString() + "%";
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
