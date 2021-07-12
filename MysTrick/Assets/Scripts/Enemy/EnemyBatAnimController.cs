61ac8ecda9b3e35c7064b11dbbe2b43870d51bb5
ZonJyaton <19cu0222@jec.ac.jp>
2021-07-12 10:58:54 +0900

    敵AI（Enemy_Bat）の作成

---
+++ MysTrick/Assets/Scripts/Enemy/EnemyBatAnimController.cs
@@ -0,0 +1,24 @@
+﻿//-------------------------------------------------
+// ファイル名		：EnemyBatAnimController.cs
+// 概要				：バットエネミーのアニメーションの制御
+// 作成者			：鍾家同
+// 更新内容			：2021/07/09 作成
+//-------------------------------------------------
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBatAnimController : MonoBehaviour
+{
+
+	void Awake()
+	{
+		
+	}
+
+
+	void Update()
+	{
+		
+	}
+}
