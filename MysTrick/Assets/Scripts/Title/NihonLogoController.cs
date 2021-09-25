38ae2ee912a421f77811ed27abd0eb0f448b8133
Zon Jyaton <19cu0222@jec.ac.jp>
2021-09-25 22:46:51 +0900

    NihonPVの追加

---
+++ MysTrick/Assets/Scripts/Title/NihonLogoController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class NihonLogoController : MonoBehaviour
+{
+	VideoPlayer videoPlayer;
+
+	void Awake()
+	{
+		videoPlayer = GetComponent<VideoPlayer>();
+	}
+
+	void Start()
+	{
+		videoPlayer.loopPointReached += LoopPointReached;
+		videoPlayer.Play();
+	}
+
+	public void LoopPointReached(VideoPlayer vp)
+	{
+		SceneManager.LoadScene("Title");
+		videoPlayer.Stop();
+	}
+}
